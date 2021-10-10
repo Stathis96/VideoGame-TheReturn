4cb45ef3f059d780bad95dd2e6fb9a5ca944b918
Stathis96 <ioannidis.stathis@yahoo.gr>
2021-10-10 19:11:04 +0300

    Completed Project
    Successfully completed and passed the Module!
    Have been awarded with 2 ECTS for it.

---
+++ Assets/Scripts/Actions/Help.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Actions/Help")]
+public class Help : Action
+{
+    public override void RespondToInput(GameController controller, string verb)
+    {
+        controller.currentText.text = "Type a Verb followed by a noun(e.g. \"go north\")";
+        controller.currentText.text += "\nAllowed verbs:\nGo, Examine, Get, Give, Use, Inventory, TalkTo, Say, Help, Read";
+    }
+}
